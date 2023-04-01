7499cf5096d810a8815574e98a3d6c9face07b34
ViTorri <45861551+ViTorri@users.noreply.github.com>
2023-04-02 00:32:15 +0100

    1st task is done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,40 @@
-// Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
 console.log(galleryItems);
+
+function createGalleryMarkup(images) {
+    return images.map(({ preview, original, description }) => {
+        return `<li class="gallery__item">
+   <a class="gallery__link" data-lightbox="gallery" onclick="event.preventDefault()" href="${original}">
+      <img class="gallery__image lightbox" 
+      src="${preview}" 
+      alt="${description}" />
+   </a>
+</li>`
+    }).join("");
+}
+
+console.log(createGalleryMarkup(galleryItems));
+
+const gallery = document.querySelector('.gallery');
+const imageMarkup = createGalleryMarkup(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', imageMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+gallery.addEventListener('click', onImageClick);
+
+function onImageClick(event) {
+    if (!event.target.classList.contains("gallery__image")) {
+        return;
+    }
+    console.log(event.target.src);
+
+};
